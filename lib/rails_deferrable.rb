ea46a8d73d7af41c053348d12abcfb9292ae3b7e
Kieran Klaassen <kieranklaassen@gmail.com>
2020-08-11 14:41:44 -0500

    feat: initial commit

---
+++ lib/rails_deferrable.rb
@@ -0,0 +1,6 @@
+require "rails_deferrable/version"
+
+module RailsDeferrable
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rails_deferrable/version.rb
@@ -0,0 +1,3 @@
+module RailsDeferrable
+  VERSION = "0.1.0"
+end
